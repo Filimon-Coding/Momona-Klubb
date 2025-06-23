3a34a042eacf94228d1ec8e15ff3959664022da6
Filimon-Coding <filimon.nuguse22@gmail.com>
2025-06-23 21:03:59 +0200

    Adde navigation bar and link routers on the router task

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 import Menu from './pages/menu';
 import User from './pages/user';
 import Test from './pages/test'; 
 
+
 function App() {
   return (
     <Router>
+      <nav style={{ padding: '10px', backgroundColor: '#f0f0f0' }}>
+  <Link to="/menu" style={{ marginRight: '15px' }}>Menu</Link>
+  <Link to="/user" style={{ marginRight: '15px' }}>User</Link>
+  <Link to="/test">Test</Link>
+</nav>
+
+
       <Routes>
         <Route path="/menu" element={<Menu />} />
         <Route path="/user" element={<User />} />
