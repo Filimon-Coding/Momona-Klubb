2fd0b259ac47f3f65dbbaaeb1e3618f7f8b37f5b
Filimon-Coding <filimon.nuguse22@gmail.com>
2025-07-06 13:15:59 +0200

    opplasting av filer

--- backend/MomonaApi/obj/Debug/net8.0/MomonaApi.AssemblyInfo.cs
+++ backend/MomonaApi/obj/Debug/net8.0/MomonaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MomonaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+190bdb4e2989125f7fd2c65beea4df5b6adf1d09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22cd88692b3b872eb649e648a88fef3f88970749")]
 [assembly: System.Reflection.AssemblyProductAttribute("MomonaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MomonaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
